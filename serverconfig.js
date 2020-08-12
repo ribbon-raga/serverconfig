cb47746627774ceabfc092c5738a7d7b6b0c7f4b
rganeshan <rganeshan@rbbn.com>
2020-08-12 01:12:44 -0400

    Commit 2

---
+++ serverconfig.js
@@ -0,0 +1,34 @@
+module.exports = function(RED) {
+	
+    function ServerConfig(config) {
+        RED.nodes.createNode(this,config);
+					
+		this.name = config.name || "Server Config";
+		this.container = config.container || "payload";
+        this.service = config.service || "sms" ;		
+		this.runner = config.runner || "" ;
+				
+		let node = this;		
+			
+		let msg = {};
+		
+        node.on("input", function(msg) {	
+		
+		let testSpec = { 		
+		"runnerURL": node.runner,
+		"service": node.service
+		}
+			
+		msg.testSpec = testSpec;
+				
+		node.status({
+			fill: "green",
+			shape: "dot",
+			text: `Test Specification captured`
+		});
+						
+		node.send(msg);
+        });
+    }
+    RED.nodes.registerType("serverconfig",ServerConfig);
+};
