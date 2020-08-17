53816d530ab4a92d7b73ec21762d808ab80b118a
rganeshan <rganeshan@rbbn.com>
2020-08-16 22:42:54 -0400

    moved category, removed contatiner

--- serverconfig.js
+++ serverconfig.js
@@ -1,34 +1,33 @@
 module.exports = function(RED) {
-	
+
     function ServerConfig(config) {
-        RED.nodes.createNode(this,config);
-					
-		this.name = config.name || "Server Config";
-		this.container = config.container || "payload";
-        this.service = config.service || "sms" ;		
-		this.runner = config.runner || "" ;
-				
-		let node = this;		
-			
-		let msg = {};
-		
-        node.on("input", function(msg) {	
-		
-		let testSpec = { 		
-		"runnerURL": node.runner,
-		"service": node.service
-		}
-			
-		msg.testSpec = testSpec;
-				
-		node.status({
-			fill: "green",
-			shape: "dot",
-			text: `Test Specification captured`
-		});
-						
-		node.send(msg);
+        RED.nodes.createNode(this, config);
+
+        this.name = config.name || "Server Config";
+        this.service = config.service || "sms";
+        this.runner = config.runner || "";
+
+        let node = this;
+
+        let msg = {};
+
+        node.on("input", function(msg) {
+
+            let testSpec = {
+                "runnerURL": node.runner,
+                "service": node.service
+            }
+
+            msg.testSpec = testSpec;
+
+            node.status({
+                fill: "green",
+                shape: "dot",
+                text: `Test Specification captured`
+            });
+
+            node.send(msg);
         });
     }
-    RED.nodes.registerType("serverconfig",ServerConfig);
+    RED.nodes.registerType("serverconfig", ServerConfig);
 };
